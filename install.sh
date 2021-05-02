a216c5e116587385bd17b7367f0b40009add11ec
Venz-ID <83541259+Venz-ID@users.noreply.github.com>
2021-05-02 20:15:05 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+pkg upgrade
+pkg update
+pip install --upgrade pip
+pip2 install --upgrade pip
+pip  install mechanize
+pip2 install mechanize
+pip2 install requests
+pip install requests
+pkg install python
+pkg install python2
+
+
